5ba940c3e6a052a8ca90327358a2d9588cb0955b
Miguel Espina <miguelangel_43@outlook.com>
2021-11-21 12:00:29 +0100

    Added casting of numerical columns to int

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-# Run it with /Users/espina/Unsynced/spark-3.1.2-bin-hadoop3.2/bin/spark-submit "/Users/espina/Documents/MSc Data Science/Big Data: Data Visualization/spark_assignment/app.py"
+# Run it with /Users/espina/Unsynced/spark-3.1.2-bin-hadoop3.2/bin/spark-submit "/Users/espina/Documents/MSc Data Science/Big Data: Data Visualization/Spark-Assignment/app.py"
 
 import sys
 from pyspark_dist_explore import hist
@@ -8,6 +8,10 @@ import matplotlib.pyplot as plt
  
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
+# Packages to one-hot encode categorical vars
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import StringIndexer
  
 if __name__ == "__main__":
 
@@ -18,19 +22,39 @@ if __name__ == "__main__":
 
     # Read the dataset into a spark dataframe
     df = spark.read.csv(path, header=True)
+    # Get only a sample of the rows for faster computation
+    df = df.limit(1000)
+    print(df.count())
     df.printSchema()
-    #print(data.count())
-    # print(df.schema.names)
 
     # Drop the forbidden columns
     # 1. Several variables may not contain useful information or are forbidden. These need to be filtered out.
     forbidden_vars = ('ArrTime', 'ActualElapsedTime', 'AirTime', 'TaxiIn', 'Diverted', 'CarrierDelay',
      'WeatherDelay', 'NASDelay', 'SecurityDelay', 'LateAircraftDelay')
     df = df.drop(*forbidden_vars)
-    df.printSchema()
 
-    # 2. Several variables may contain information in a way that is difficult to understand/process. 
-    # These need to be transformed into something meaningful.
 
-    # 3. Several variables may provide better information when combined with others. In these cases, 
-    # new variables could be derived from them.
+    # # Cast columns datatypes to adequate one
+    # # Cast to int these numerical columns
+    df = df.withColumn("Year",col("Year").cast("int"))
+    df = df.withColumn("Month",col("Month").cast("int"))
+    df = df.withColumn("DayofMonth",col("DayofMonth").cast("int"))
+    df = df.withColumn("DayOfWeek",col("DayOfWeek").cast("int"))
+    df = df.withColumn("DepTime",col("DepTime").cast("int"))
+    df = df.withColumn("CRSDepTime",col("CRSDepTime").cast("int"))
+    df = df.withColumn("CRSArrTime",col("CRSArrTime").cast("int"))
+    df = df.withColumn("FlightNum",col("FlightNum").cast("int")) # Irrelevant variable?
+    df = df.withColumn("CRSElapsedTime",col("CRSElapsedTime").cast("int"))
+    df = df.withColumn("ArrDelay",col("ArrDelay").cast("int"))
+    df = df.withColumn("DepDelay",col("DepDelay").cast("int"))
+    df = df.withColumn("Distance",col("Distance").cast("int"))
+    df = df.withColumn("TaxiOut",col("TaxiOut").cast("int"))
+    df = df.withColumn("Cancelled",col("Cancelled").cast("int"))
+
+    # Apply StringIndexer to the categorical columns
+    cat_columns = ["UniqueCarrier", "TailNum", "Origin", "Dest", "CancellationCode"]
+    indexers = [StringIndexer(inputCol=column, outputCol=column+"_index").fit(df) for column in cat_columns]
+    pipeline = Pipeline(stages=indexers)
+    df_r = pipeline.fit(df).transform(df)
+    df_r.printSchema()
+    print(df_r.head(1))
