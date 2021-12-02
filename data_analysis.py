7c33a7224c927a45b21094c8d1961faab6214c55
Miguel Espina <miguelangel_43@outlook.com>
2021-12-02 13:33:22 +0100

    file for exploratory data analysis created

---
+++ data_analysis.py
@@ -0,0 +1,16 @@
+# Function(s) for exploratory analysis of the data and to determine the variable selection
+
+def print_basic_stats(df):
+    """
+    Prints basic stats of the dataframe
+
+    :param df
+    """
+
+def select_variables(df):
+    """
+    Analyzes the variables (correlation...) and returns the optimal to run the ml algorithms on
+
+    :param df
+    :return list of variable names as string 
+    """
