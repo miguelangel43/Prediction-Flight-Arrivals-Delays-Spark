83dd7c0faabaa9a529263efe6dfb588e97e16502
pablocabargas <pablo.cabargas@gmail.com>
2021-12-02 19:25:02 +0100

    added tunning class

--- classifiers/tunning.py
+++ classifiers/tunning.py
@@ -2,21 +2,23 @@
 
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.evaluation import RegressionEvaluator 
-from pyspark.ml.tuning import ParamGridBuilder
+from pyspark.ml.tuning import ParamGridBuilder, CrossValidator
+from pyspark.ml.regression import LinearRegression
 
 
 
-    test_object = Tunning()
-    test_object.run()
+
 
 class Tunning:
 
-	def __init__(self):
-        self.model = None
+	def __init__(self,data):
+		self.data = data
+        
 
-    def run(self,data):
+	def run(self):
 
-    	lr = LogisticRegression(maxIter=10)
+		#lr = LogisticRegression(maxIter=10)
+		lr = LinearRegression(featuresCol = 'features', labelCol='ArrDelay', maxIter=10)
 
 		paramGrid = ParamGridBuilder() \
     	.addGrid(lr.regParam, [0.1, 0.01]) \
@@ -27,7 +29,7 @@ class Tunning:
                           evaluator=RegressionEvaluator(),
                           numFolds=3) 
 
-		cvModel = crossval.fit(training)
+		cvModel = crossval.fit(self.data)
 
 		print(cvModel.explainParams())
 
