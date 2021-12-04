fb7435032feebb3feb80b6362186d4caec1543fe
pablocabargas <pablo.cabargas@gmail.com>
2021-12-05 00:17:55 +0100

    version with testing of 3 models

--- classifiers/tunning.py
+++ classifiers/tunning.py
@@ -16,28 +16,28 @@ class Tunning:
 
 	def run_lr(self):
 
-		#lr = LogisticRegression(maxIter=10)
+		# Define Linear Regression Model with basic definitions
 		lr = LinearRegression(featuresCol = 'features', labelCol='label')
 
-
-		#print("#########Linear regression default parameters#########")
-		#print(lr.params)
-		#print("#########END Linear regression parameters#########")
-
-
+		# Initialize Grid of hyperparameters to explore
 		paramGrid = ParamGridBuilder() \
     	.addGrid(lr.regParam, [0.01, 0.1,1,10,100]) \
     	.addGrid(lr.elasticNetParam,[0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]) \
     	.addGrid(lr.maxIter,[10,100,500]) \
     	.build()
 
+    	# Initialize CrossValidator that will use the model, the grid of hyperparameters and a RegressionEvaluator
 		crossval = CrossValidator(estimator=lr,
                           estimatorParamMaps=paramGrid,
                           evaluator=RegressionEvaluator(),
                           numFolds=3) 
 
+		# Fit Model on different scenarios for the Grid
 		cvModel = crossval.fit(self.df)
+
+		# Select the best Model
 		bestModel = cvModel.bestModel
+
 
 		print("###############Parameters Linear Regression ################")
 		print("-- Tried:")
@@ -46,15 +46,14 @@ class Tunning:
 		print("----- maxIter:",[10,100,500])
 
 
-
+		# Parameters of the best model
 		print("Best Parameters")
 		print(bestModel._java_obj.extractParamMap())
 
-		# print(cvModel.explainParams())
 
 		print("coefficients and intercept")
 
-		# # Print the coefficients and intercept for linear regression
+		# Print the coefficients and intercept for linear regression
 		print("Coefficients: %s" % str(bestModel.coefficients))
 		print("Intercept: %s" % str(bestModel.intercept))
 
@@ -62,28 +61,23 @@ class Tunning:
 
 	def run_dt(self):
 
-		#lr = LogisticRegression(maxIter=10)
-		#lr = LinearRegression(featuresCol = 'features', labelCol='label')
+		# Define Decission Tree Regresor with basic parameters
 		dt = DecisionTreeRegressor(featuresCol='features', labelCol='label')
 
-		#print("#########decision tree default parameters#########")
-		#print(dt.params)
-		#print("#########END decision tree parameters#########")
-		print("metric",RegressionEvaluator().getMetricName())
-
-
+		# Initialize Grid of hyperparameters to explore
 		paramGrid = ParamGridBuilder() \
     	.addGrid(dt.minInstancesPerNode, [10,50,100,500]) \
     	.addGrid(dt.maxDepth,[5,10,15,20,25,30]) \
     	.build() 
 
     	
-
+    	# Initialize CrossValidator that will use the model, the grid of hyperparameters and a RegressionEvaluator
 		crossval = CrossValidator(estimator=dt,
                           estimatorParamMaps=paramGrid,
                           evaluator=RegressionEvaluator(),
                           numFolds=3) 
 
+		# Fit Model on different scenarios for the Grid
 		cvModel = crossval.fit(self.df)
 		bestModel = cvModel.bestModel
 
@@ -93,29 +87,25 @@ class Tunning:
 		print("----- maxDepth:",[5,10,15,20,25,30])
 
 		
-	
-		#print("Summarize the model over the training set and print out some metrics")
-		#print(bestModel)
-		#print("End summarize")
-
+		# Parameters of the best model
 		print("Best Parameters for decision tree")
 		print(bestModel._java_obj.extractParamMap())
 
 		return bestModel
 
 	def run_rf(self):
+
+		# Define Random Forest with basic parameters
 		rf = RandomForestRegressor(featuresCol='features', labelCol='label')
 
-		#print("#########random forest  default parameters#########")
-		#print(rf.params)
-		#print("#########END random forest parameters#########")
-
-
+		# Initialize Grid of hyperparameters to explore
 		paramGrid = ParamGridBuilder() \
     	.addGrid(rf.numTrees, [3,5,10]) \
     	.addGrid(rf.maxDepth,[5,10,15,20,25,30]) \
     	.build() #max depth allowed 30
 
+
+    	# Initialize CrossValidator that will use the model, the grid of hyperparameters and a RegressionEvaluator
 		crossval = CrossValidator(estimator=rf,
                           estimatorParamMaps=paramGrid,
                           evaluator=RegressionEvaluator(),
@@ -124,17 +114,14 @@ class Tunning:
 		cvModel = crossval.fit(self.df)
 		bestModel = cvModel.bestModel
 
+
 		print("###############Parameters Random Forest ################")
 		print("-- Tried:")
 		print("----- numTres:",[3,5,10])
 		print("----- maxDepth:",[5,10,15,20,25,30])
 
 
-		
-		#print("Summarize the model over the training set and print out some metrics")
-		#print(bestModel)
-		#print("End summarize")
-
+		# Parameters of the best model
 		print("Best Parameters for random forest")
 		print(bestModel._java_obj.extractParamMap())
 
